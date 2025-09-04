689d74a7bc3d5469643b90af74dc7eb2bb71c486
Vaishnavi Suresh <vaishnavisv97@gmail.com>
2025-09-05 02:09:47 +0530

    Removed everything except src

--- src/service/PaymentService.js
+++
@@ -1,9 +0,0 @@
-import axiosInstance from "../http-common";
-
-const PaymentService = {
-    makePayment: async (paymentDto) => {
-    return axiosInstance.post("/payment/make", paymentDto);
-  },
-};
-
-export default PaymentService;
